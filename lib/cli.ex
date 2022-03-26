cf8202fcb1a65bfd5a6ec173b2eabc5db7a5f873
Jim Kirkbride <james.kirkbride@gmail.com>
2022-03-26 10:54:14 -0400

    Adding an entrypoint and making the project compile to an executable.

---
+++ lib/cli.ex
@@ -0,0 +1,20 @@
+defmodule GeocodeExperiment.CLI do
+  def main(args \\ []) do
+    args
+    |> parse_args()
+    |> response()
+    |> IO.puts()
+  end
+
+  defp parse_args(args) do
+    {opts, word, _} =
+      args
+      |> OptionParser.parse(switches: [upcase: :boolean])
+
+    {opts, List.to_string(word)}
+  end
+
+  defp response({_opts, word}) do
+    word
+  end
+end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule GeocodeExperiment.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: escript()
     ]
   end
 
@@ -16,6 +17,10 @@ defmodule GeocodeExperiment.MixProject do
     [
       extra_applications: [:logger]
     ]
+  end
+
+  defp escript do
+    [main_module: GeocodeExperiment.CLI]
   end
 
   # Run "mix help deps" to learn about dependencies.
