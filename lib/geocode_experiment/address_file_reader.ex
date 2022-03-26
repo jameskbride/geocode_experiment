0beeb072ca8d5fa6bddc22839e238e741a7c35f8
Jim Kirkbride <james.kirkbride@gmail.com>
2022-03-26 11:34:04 -0400

    Handling invalid file.

--- lib/geocode_experiment/address_file_reader.ex
+++ lib/geocode_experiment/address_file_reader.ex
@@ -1,10 +1,14 @@
 defmodule GeocodeExperiment do
   defmodule AddressFileReader do
     def read_address_file(file_name) do
-      {:ok, contents} = File.read(file_name)
-      contents
-      |> String.split("\n", trim: true)
-      |> Enum.map(&IO.puts/1)
+      case File.read(file_name) do
+        {:ok, contents} ->
+          contents
+          |> String.split("\n", trim: true)
+          |> Enum.map(&IO.puts/1)
+        {:error, _} ->
+          IO.puts("Invalid file")
+      end
     end
   end
 end
