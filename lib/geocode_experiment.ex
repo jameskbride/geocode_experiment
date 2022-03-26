3676696ab32a21608648593972a9ffe1323a288e
Jim Kirkbride <james.kirkbride@gmail.com>
2022-03-26 10:42:26 -0400

    Initial commit.

---
+++ lib/geocode_experiment.ex
@@ -0,0 +1,18 @@
+defmodule GeocodeExperiment do
+  @moduledoc """
+  Documentation for `GeocodeExperiment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GeocodeExperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/geocode_experiment_test.exs
@@ -0,0 +1,8 @@
+defmodule GeocodeExperimentTest do
+  use ExUnit.Case
+  doctest GeocodeExperiment
+
+  test "greets the world" do
+    assert GeocodeExperiment.hello() == :world
+  end
+end
